5f9c7f54ced876995cd1d9f6c5e7eb2249dbc064
Francesco Ferrara <francesco.ferrara@helbling.ch>
2017-12-01 15:47:33 +0100

    Now able to set attributes as well

--- reflection.py
+++ reflection.py
@@ -16,34 +16,26 @@ def import_mod(name, pkg=None):
     outputs:
     - ret: imported module (None, if exception occurs)
     """
-    # Debug
-    end = ""
-    if __debug__:
-        end = "\n"
-
     # Initialize
     ret = None
     if pkg is None:
         pkg_str = " "
     else:
-        pkg_str = " in package %s "
+        pkg_str = " in package {} ".format(pkg)
 
     # Import the module
     try:
         ret = import_module(name, pkg)
-        output = ""
         if __debug__:
-            output = "Success! Module %s" + pkg_str + "imported." + end
+            output = "Success! Module {}".format(name) + pkg_str + "imported."
     except ImportError:
-        output = "Fail! Module %s" + pkg_str + "not imported." + end
+        output = "Fail! Module {}".format(name) + pkg_str + "not imported."
         if __debug__:
             traceback.print_stack()
             traceback.print_exc()
     finally:
-        if pkg is None:
-            print output %name,
-        else:
-            print output %(name, pkg),
+        if __debug__ or ret is None:
+            print output
 
     return ret
 
@@ -56,27 +48,22 @@ def get_attr(name, obj):
     outputs:
     - ret: attribute of the object (None, if exception occurs)
     """
-    # Debug
-    end = ""
-    if __debug__:
-        end = "\n"
-
     # Initialize
     ret = None
 
     # Get the attribute
     try:
         ret = getattr(obj, name)
-        output = ""
         if __debug__:
-            output = "Success! Attribute %s in object %s found." + end
+            output = "Success! Attribute {} in object {} found.".format(name, obj)
     except TypeError:
-        output = "Fail! Attribute %s in object %s not found." + end
+        output = "Fail! Attribute {} in object {} not found.".format(name, obj)
         if __debug__:
             traceback.print_stack()
             traceback.print_exc()
     finally:
-        print output %(name, obj),
+        if __debug__ or ret is None:
+            print output
 
     return ret
 
@@ -90,28 +77,14 @@ def set_attr(name, obj, value):
     outputs:
     -
     """
-    # Initialize
-    ret = None
-
-    # set the attribute
-    try:
-        ret = setattr(obj, name, value)
-        output = "Success! Attribute %s in object %s set."
-    except AttributeError:
-        output = "Fail! Attribute %s in object %s not set."
-        traceback.print_stack()
-        traceback.print_exc()
-    finally:
-        print output %(name, obj)
-
-    return ret
+    setattr(obj, name, value)
 
 
 # Test the library
 if __name__ == "__main__":
     # Import nonexisting module
     BLA_MOD = import_mod(".bla", "pkg.sub_pkg")
-    assert BLA_MOD, None
+    assert BLA_MOD is None
 
     # Import test module
     TEST_MOD = import_mod(".test_ref", "pkg.sub_pkg")
@@ -119,7 +92,7 @@ if __name__ == "__main__":
 
     # Get non existent attribute
     TEST_ATTR = get_attr(TEST_MOD, "nonExistent")
-    assert TEST_ATTR, None
+    assert TEST_ATTR is None
 
     # Get test class
     TEST_ATTR = get_attr("testClass", TEST_MOD)
@@ -127,15 +100,10 @@ if __name__ == "__main__":
     TEST_CLASS.printout()
     assert TEST_CLASS.text == "This is a test."
 
-    # Set non existent attribute
-    set_attr("nonExistent", "nonExistent", 5)
-
-    # Set an attribute with the wrong data
-    set_attr(TEST_ATTR, "text", 5)
-
     # Set an attribute correctly
-    set_attr(TEST_ATTR, "text", "This is a text set through reflection.")
+    set_attr("text", TEST_CLASS, "This is a text set through reflection.")
     TEST_CLASS.printout()
+    assert TEST_CLASS.text == "This is a text set through reflection."
 
     # Import ROS Pose message
     GEOMETRY_MSGS = import_mod("._Pose", "geometry_msgs.msg")
